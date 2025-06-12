e316cb20fea8fa394f92f359d6cb9f15912ca61e
thanhhuynh1903 <thanhgaming1234@gmail.com>
2025-06-12 16:14:28 +0700

    add portfolio svg

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/svg+xml" href="/portfolio.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
           rel="stylesheet"
